b3aad2aaff41455d08ea0ac622f0b4690f58f6f9
DEADPOOY <jvertiza2400@alumno.ipn.mx>
2026-05-31 00:42:23 -0600

    proyecto escuela

---
+++ HOTEL_MANAGER2/src/main/java/iv9_e1_hotman/hotel_manager2/HOTEL_MANAGER2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package iv9_e1_hotman.hotel_manager2;
+
+/**
+ *
+ * @author deadpooy
+ */
+public class HOTEL_MANAGER2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
